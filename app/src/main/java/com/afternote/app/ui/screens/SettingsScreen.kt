3b1b7d6664abdcf335c0d9b6ee22e5563e352a40
Deepanjan008 <deepanjanbiswas13@gmail.com>
2026-03-16 14:02:17 +0530

    Fix: Added missing ExperimentalFoundationApi import

--- app/src/main/java/com/afternote/app/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/afternote/app/ui/screens/SettingsScreen.kt
@@ -3,6 +3,7 @@ package com.afternote.app.ui.screens
 
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
